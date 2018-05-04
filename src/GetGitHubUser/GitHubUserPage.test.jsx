c921129d227e3ce96d0d9abd7709e4c7a9854dc9
Mark Winterbottom <mark@londonappdeveloper.com>
2018-05-04 20:09:19 +0100

    Got promise resolving testing working.

--- src/GetGitHubUser/GitHubUserPage.test.jsx
+++ src/GetGitHubUser/GitHubUserPage.test.jsx
@@ -5,16 +5,21 @@ import { shallow } from 'enzyme';
 import GitHubUserPage from './GitHubUserPage';
 
 
-it('sets isLoading to true when loading', () => {
+function flushPromises() {
+  return new Promise(resolve => setImmediate(resolve));
+}
+
+
+it('sets isLoading to true when loading', async () => {
   global.fetch = jest.fn().mockReturnValue(Promise.resolve({
     ok: true,
     json: () => Promise.resolve({'user': 'test'})
   }));
 
   const c = shallow(<GitHubUserPage />);
+  c.find('GetGitHubUserForm').prop('onClick')();
 
-  c.instance().getUser().then(() => {
-    expect(c.instance().state.isLoading).toEqual(false);
-  });
   expect(c.instance().state.isLoading).toEqual(true);
+  await flushPromises();
+  expect(c.instance().state.isLoading).toEqual(false);
 });
